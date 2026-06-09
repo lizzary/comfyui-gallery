ffef9db0550d1a94d95d67ee239fc7e3465b9696
lizzary <2962354027@qq.com>
2026-06-09 16:27:32 +0800

    bug fix: Fixed some bugs that occurred during the packaging process when running (add .spec)

--- backend/main.py
+++ backend/main.py
@@ -720,3 +720,5 @@ if __name__ == "__main__":
         uvicorn.run(app, host="127.0.0.1", port=8000, reload=False)
     else:
         uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
+#package: pyinstaller --clean backend.spec
